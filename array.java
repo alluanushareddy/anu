acc1477d0b231e89e7089c3d1a396954ce1daeee
Anusha Allu <alluanushareddy4@gmail.com>
2018-06-18 18:34:07 -0500

    arraylist

---
+++ array.java
@@ -0,0 +1,10 @@
+class Lists
+{
+	public static void main (String args[])
+	{
+		Arraylist<String>  al = new Arraylist<String>();
+		al.add("anu");
+		al.add("sweety");
+		System.out.println(al);
+	}
+}
